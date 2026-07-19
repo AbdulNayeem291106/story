91e9d61366e8cd5413c9f9fb4700bf91308baa5d
Abdul . Nayeem <nayeem.abdul.2829@gmail.com>
2026-07-19 21:13:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -673,8 +673,7 @@ function showBirthdayTitle(){
         scenes.birthday    
     );    
     
-    createConfetti();    
-    
+    createConfetti();
     
     
     setTimeout(()=>{    
@@ -847,44 +846,9 @@ setBirthdayName(birthdayName);
     
 function createConfetti(){    
     
-    
-  function createConfetti(container){
+    const container =
+document.querySelector(".confetti-container");
 
-    if(!container) return;
-
-    container.innerHTML = "";
-
-    const colors = [
-        "#ff4d6d",
-        "#ffd166",
-        "#06d6a0",
-        "#118ab2",
-        "#8338ec",
-        "#ffffff"
-    ];
-
-    for(let i=0;i<80;i++){
-
-        const piece = document.createElement("div");
-
-        piece.className = "confetti";
-
-        piece.style.left = Math.random()*100 + "%";
-
-        piece.style.animationDuration =
-            (3 + Math.random()*4) + "s";
-
-        piece.style.animationDelay =
-            Math.random()*3 + "s";
-
-        piece.style.background =
-            colors[Math.floor(Math.random()*colors.length)];
-
-        piece.style.transform =
-            `rotate(${Math.random()*360}deg)`;
-
-        container.appendChild(piece);
-    }
 }
 
 containers.forEach(container => {
